b33f320ad86dfc1ac7a50c62c36d10fbda663fe6
amzadhossainjacky <amzadhossainjacky@gmail.com>
2026-05-06 16:48:32 +0600

    ss2

--- app/page.js
+++ app/page.js
@@ -12,7 +12,6 @@ export default function Home() {
           >
             CI CD with Jenkins and Next.js !! Jacky the great  works hard to make this project successful.
             2026 will be lengary year in ai and devops world. Let's make it happen together.
-            great things are coming in 2026. Stay tuned for more updates and exciting projects!!
 
           </a>
         </h1>
